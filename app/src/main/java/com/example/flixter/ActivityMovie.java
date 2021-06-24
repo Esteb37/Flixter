a61c7ede92ea6c1f307d118254a82269bf0fd171
Esteb37 <esteban37padilla@gmail.com>
2021-06-24 00:10:43 -0500

    Added related movies

--- app/src/main/java/com/example/flixter/ActivityMovie.java
+++ app/src/main/java/com/example/flixter/ActivityMovie.java
@@ -37,15 +37,18 @@ public class ActivityMovie extends AppCompatActivity {
         TextView tvTime = findViewById(R.id.tvTime);
         ImageView ivPoster = findViewById(R.id.ivPoster2);
         RatingBar rbRating = findViewById(R.id.rbRating);
-
         String TAG = "MainActivity";
+        ImageView similar1 = findViewById((R.id.similar1));
+        ImageView similar2 = findViewById((R.id.similar2));
+        ImageView similar3 = findViewById((R.id.similar3));
 
         AsyncHttpClient client = new AsyncHttpClient();
 
         int id = getIntent().getExtras().getInt("id");
 
         final String MOVIE_URL = String.format("https://api.themoviedb.org/3/movie/%s?api_key=9ace9cb9774ff085d83b43b0724903f6&language=en-US", id);
-        final String SIMILAR_URL = String.format("https://api.themoviedb.org/3/movie/%s?api_key=9ace9cb9774ff085d83b43b0724903f6&language=en-US", id);
+        final String SIMILAR_URL = String.format("https://api.themoviedb.org/3/movie/%s/similar?api_key=9ace9cb9774ff085d83b43b0724903f6&language=en-US&page=1", id);
+
         //Create request for the movie catalogue
         client.get(MOVIE_URL, new JsonHttpResponseHandler() {
 
@@ -107,11 +110,72 @@ public class ActivityMovie extends AppCompatActivity {
 
             }
 
+
+
             //If the request is not successfully
             @Override
             public void onFailure(int i, Headers headers, String s, Throwable throwable) {
                 Log.d(TAG, "onFailure");
             }
         });
+
+
+        client.get(SIMILAR_URL, new JsonHttpResponseHandler() {
+
+            //If the request is successfully
+            @Override
+            public void onSuccess(int i, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+
+                //Get movie request as JSON object;
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray similar = jsonObject.getJSONArray("results");
+
+
+                    try {
+                        JSONObject jsonSimilar1 = (JSONObject) similar.get(0);
+                        Glide.with(context).load(String.format("https://image.tmdb.org/t/p/w342/%s",jsonSimilar1.getString("poster_path")))
+                                .placeholder(R.mipmap.placeholder)
+                                .error(R.mipmap.placeholder)
+                                .into(similar1);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        JSONObject jsonSimilar2 = (JSONObject) similar.get(1);
+                        Glide.with(context).load(String.format("https://image.tmdb.org/t/p/w342/%s",jsonSimilar2.getString("poster_path")))
+                                .placeholder(R.mipmap.placeholder)
+                                .error(R.mipmap.placeholder)
+                                .into(similar2);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        JSONObject jsonSimilar3 = (JSONObject) similar.get(2);
+                        Glide.with(context).load(String.format("https://image.tmdb.org/t/p/w342/%s",jsonSimilar3.getString("poster_path")))
+                                .placeholder(R.mipmap.placeholder)
+                                .error(R.mipmap.placeholder)
+                                .into(similar3);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+
+
+            //If the request is not successfully
+            @Override
+            public void onFailure(int i, Headers headers, String s, Throwable throwable) {
+                Log.d(TAG, "onFailure");
+            }
+        });
+
+
     }
 }
